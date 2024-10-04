ad681124ec01a65ecdeff041d1b605572dded68c
Sara Guaiume <130059301+saraqwe123@users.noreply.github.com>
2024-10-04 10:32:57 -0300

    Add files via upload

---
+++ src/components/comunicacao/indireta/PaiIndireta.jsx
@@ -0,0 +1,12 @@
+
+export function PaiIndireta() {
+    return (
+        <div className={"w-full flex flex-col items-center p-7 rounded-md bg-custom-gray text-custom-white"}>
+            <div className="flex gap-3">
+                <button className="botao">João</button>
+                <button className="botao">Sara mozao</button>
+                <button className="botao">Pedro</button>
+            </div>
+        </div>
+    )
+}
